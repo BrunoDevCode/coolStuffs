b686c3d2c76a679873f261dea838879249a4c40f
Bruno Henrique Santos <brunohenriquesantos272@gmail.com>
2020-08-19 13:02:12 -0300

    Adiciona tipos de arrow functions

---
+++ arrowFunctions/code.js
@@ -0,0 +1,15 @@
+// Tradicional
+
+function soma0(a, b) {
+  return a + b
+}
+
+// Arrow Function
+
+const soma1 = (a + b) => {
+  return a + b
+}
+
+// Sintaxe Curta
+
+const soma2 = (a, b) => a + b;
